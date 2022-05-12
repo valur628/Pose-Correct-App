3704b192d4547eabd9e5803519e9624e105f310d
valurauta <valurauta628@gmail.com>
2022-05-12 19:07:31 +0900

    8

---
+++ app/src/main/java/com/posemobile/correctpose/MainActivity.java
@@ -0,0 +1,353 @@
+package com.posemobile.correctpose;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.SurfaceTexture;
+
+import android.util.Log;
+import android.util.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
+import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
+import com.google.mediapipe.components.CameraHelper;
+import com.google.mediapipe.components.CameraXPreviewHelper;
+import com.google.mediapipe.components.ExternalTextureConverter;
+import com.google.mediapipe.components.FrameProcessor;
+import com.google.mediapipe.components.PermissionHelper;
+import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.framework.AndroidPacketCreator;
+import com.google.mediapipe.framework.PacketGetter;
+import com.google.mediapipe.framework.Packet;
+import com.google.mediapipe.glutil.EglManager;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Main activity of MediaPipe example apps.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String BINARY_GRAPH_NAME = "pose_tracking_gpu.binarypb";
+    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
+    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
+    private static final String OUTPUT_LANDMARKS_STREAM_NAME = "pose_landmarks";
+    private static final int NUM_FACES = 1;
+    private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
+    // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
+    // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
+    // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
+    // corner, whereas MediaPipe in general assumes the image origin is at top-left.
+    private static final boolean FLIP_FRAMES_VERTICALLY = true;
+
+    static {
+        // Load all native libraries needed by the app.
+        System.loadLibrary("mediapipe_jni");
+        System.loadLibrary("opencv_java3");
+    }
+
+    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
+    private SurfaceTexture previewFrameTexture;
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private SurfaceView previewDisplayView;
+    // Creates and manages an {@link EGLContext}.
+    private EglManager eglManager;
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    private FrameProcessor processor;
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private ExternalTextureConverter converter;
+    // ApplicationInfo for retrieving metadata defined in the manifest.
+    private ApplicationInfo applicationInfo;
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    private CameraXPreviewHelper cameraHelper;
+
+    private TextView tv;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView tv4;
+    private TextView tv5;
+    private TextView tv6;
+
+    class markPoint {
+        float x;
+        float y;
+        float z;
+    }
+
+    private NormalizedLandmark[] bodyTempPoint = new NormalizedLandmark[33];
+    //임시 랜드마크 포인트 변수
+    private markPoint[] bodyMarkPoint = new markPoint[33];
+    //몸 랜드마크 포인트 변수
+    private float[] bodyRatioMeasurement = new float[33];
+    //비율 계산값 변수(정규화 값)
+
+    private float ratioPoint_1a, ratioPoint_1b, ratioPoint_2a, ratioPoint_2b;
+    //비율 계산에 쓰일 포인트 변수 (왼쪽, 오른쪽)
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewLayoutResId());
+        tv = findViewById(R.id.tv);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
+        tv5 = findViewById(R.id.tv5);
+        tv6 = findViewById(R.id.tv6);
+        //tv.setText("000");
+        try {
+            applicationInfo =
+                    getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            Log.e(TAG, "Cannot find application info: " + e);
+        }
+
+        //tv.setText("111");
+        previewDisplayView = new SurfaceView(this);
+        setupPreviewDisplayView();
+        //tv.setText("222");
+
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this);
+        eglManager = new EglManager(null);
+        //tv.setText("333");
+        processor =
+                new FrameProcessor(
+                        this,
+                        eglManager.getNativeContext(),
+                        BINARY_GRAPH_NAME,
+                        INPUT_VIDEO_STREAM_NAME,
+                        OUTPUT_VIDEO_STREAM_NAME);
+        processor
+                .getVideoSurfaceOutput()
+                .setFlipY(FLIP_FRAMES_VERTICALLY);
+
+        //tv.setText("444");
+        PermissionHelper.checkAndRequestCameraPermissions(this);
+        //tv.setText("555");
+        AndroidPacketCreator packetCreator = processor.getPacketCreator();
+        //tv.setText("666");
+        Map<String, Packet> inputSidePackets = new HashMap<>();
+        //tv.setText("888");
+        processor.setInputSidePackets(inputSidePackets);
+        //tv.setText("999");
+
+
+        // To show verbose logging, run:
+        // adb shell setprop log.tag.MainActivity VERBOSE
+
+        if (Log.isLoggable(TAG, Log.WARN)) {
+            processor.addPacketCallback(
+                    OUTPUT_LANDMARKS_STREAM_NAME,
+                    (packet) -> {
+                        byte[] landmarksRaw = PacketGetter.getProtoBytes(packet);
+                        try {
+                            NormalizedLandmarkList poseLandmarks = NormalizedLandmarkList.parseFrom(landmarksRaw);
+                            tv6.setText("a");
+                            ratioPoint_1a = poseLandmarks.getLandmark(11).getY() * 1920f;
+                            ratioPoint_1b = poseLandmarks.getLandmark(13).getY() * 1920f;
+                            ratioPoint_2a = poseLandmarks.getLandmark(12).getY() * 1920f;
+                            ratioPoint_2b = poseLandmarks.getLandmark(14).getY() * 1920f;
+                            tv6.setText("b");
+                            for (int i = 0; i <= 32; i++) {
+                                bodyMarkPoint[i] = new markPoint();
+                                tv6.setText("c");
+                                bodyTempPoint[i] = poseLandmarks.getLandmark(i);
+                                tv6.setText("d");
+                                bodyMarkPoint[i].x = bodyTempPoint[i].getX() * 1080f;
+                                tv6.setText("e");
+                                bodyMarkPoint[i].y = bodyTempPoint[i].getY() * 1080f;
+                                tv6.setText("f");
+                                bodyMarkPoint[i].z = bodyTempPoint[i].getZ() * 1080f;
+                                tv6.setText("g");
+                                bodyRatioMeasurement[i] = bodyMarkPoint[i].x / (ratioPoint_1b - ratioPoint_1a);
+                                tv6.setText("h");
+                                bodyRatioMeasurement[i] = bodyMarkPoint[i].y / (ratioPoint_1b - ratioPoint_1a);
+                                tv6.setText("i");
+                                bodyRatioMeasurement[i] = bodyMarkPoint[i].z / (ratioPoint_1b - ratioPoint_1a);
+                                tv6.setText("k");
+                            }
+
+                            tv6.setText("1");
+                            tv.setText(bodyMarkPoint[13].x + " = 13X / 13Y = " + bodyMarkPoint[13].y);
+                            tv6.setText("2");
+                            tv2.setText(bodyMarkPoint[13].z + " = 13Z / RatioZ = " + bodyRatioMeasurement[13]);
+                            tv6.setText("3");
+                            tv3.setText(bodyMarkPoint[14].x + " = 14X / 14Y = " + bodyMarkPoint[14].y);
+                            tv6.setText("4");
+                            tv4.setText(bodyMarkPoint[14].z + " = 14Z / RatioZ = " + bodyRatioMeasurement[14]);
+                            tv6.setText("5");
+                            tv5.setText(getLandmarksAngleTwo(bodyMarkPoint[12], bodyMarkPoint[14], bodyMarkPoint[16], 'x', 'y') + " =AngleXY 14 AngleXZ= " + getLandmarksAngleTwo(bodyMarkPoint[12], bodyMarkPoint[14], bodyMarkPoint[16], 'x', 'z'));
+                            tv6.setText("6");
+                        } catch (InvalidProtocolBufferException e) {
+                            Log.e(TAG, "Couldn't Exception received - " + e);
+                            return;
+                        }
+                    }
+            );
+        }
+    }
+
+    // Used to obtain the content view for this application. If you are extending this class, and
+    // have a custom layout, override this method and return the custom layout.
+    protected int getContentViewLayoutResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        converter =
+                new ExternalTextureConverter(
+                        eglManager.getContext(), 2);
+        converter.setFlipY(FLIP_FRAMES_VERTICALLY);
+        converter.setConsumer(processor);
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        converter.close();
+
+        // Hide preview display until we re-open the camera again.
+        previewDisplayView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    protected void onCameraStarted(SurfaceTexture surfaceTexture) {
+        previewFrameTexture = surfaceTexture;
+        // Make the display view visible to start showing the preview. This triggers the
+        // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+        previewDisplayView.setVisibility(View.VISIBLE);
+    }
+
+    protected Size cameraTargetResolution() {
+        return null; // No preference and let the camera (helper) decide.
+    }
+
+    public void startCamera() {
+        cameraHelper = new CameraXPreviewHelper();
+        cameraHelper.setOnCameraStartedListener(
+                surfaceTexture -> {
+                    onCameraStarted(surfaceTexture);
+                });
+        CameraHelper.CameraFacing cameraFacing = CameraHelper.CameraFacing.FRONT;
+        cameraHelper.startCamera(
+                this, cameraFacing, previewFrameTexture, cameraTargetResolution());
+    }
+
+    protected Size computeViewSize(int width, int height) {
+        return new Size(width, height);
+    }
+
+    protected void onPreviewDisplaySurfaceChanged(
+            SurfaceHolder holder, int format, int width, int height) {
+        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+        // based on the size of the SurfaceView that contains the display.
+        Size viewSize = computeViewSize(width, height);
+        Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
+        boolean isCameraRotated = cameraHelper.isCameraRotated();
+
+        // Connect the converter to the camera-preview frames as its input (via
+        // previewFrameTexture), and configure the output width and height as the computed
+        // display size.
+        converter.setSurfaceTextureAndAttachToGLContext(
+                previewFrameTexture,
+                isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
+                isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
+    }
+
+    private void setupPreviewDisplayView() {
+        previewDisplayView.setVisibility(View.GONE);
+        ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
+        viewGroup.addView(previewDisplayView);
+
+        previewDisplayView
+                .getHolder()
+                .addCallback(
+                        new SurfaceHolder.Callback() {
+                            @Override
+                            public void surfaceCreated(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
+                            }
+
+                            @Override
+                            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                                onPreviewDisplaySurfaceChanged(holder, format, width, height);
+                            }
+
+                            @Override
+                            public void surfaceDestroyed(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(null);
+                            }
+                        });
+    }
+
+    private static String getPoseLandmarksDebugString(NormalizedLandmarkList poseLandmarks) {
+        String poseLandmarkStr = "Pose landmarks: " + poseLandmarks.getLandmarkCount() + "\n";
+        int landmarkIndex = 0;
+        for (NormalizedLandmark landmark : poseLandmarks.getLandmarkList()) {
+            poseLandmarkStr +=
+                    "\tLandmark ["
+                            + landmarkIndex
+                            + "]: ("
+                            + landmark.getX()
+                            + ", "
+                            + landmark.getY()
+                            + ", "
+                            + landmark.getZ()
+                            + ")\n";
+            ++landmarkIndex;
+        }
+        return poseLandmarkStr;
+    }
+
+    public static float getLandmarksAngleTwo(markPoint p1, markPoint p2, markPoint p3, char a, char b) {
+        float p1_2 = 0f, p2_3 = 0f, p3_1 = 0f;
+        if (a == b) {
+            return 0;
+        } else if ((a == 'x' || b == 'x') && (a == 'y' || b == 'y')) {
+            p1_2 = (float) Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+            p2_3 = (float) Math.sqrt(Math.pow(p2.x - p3.x, 2) + Math.pow(p2.y - p3.y, 2));
+            p3_1 = (float) Math.sqrt(Math.pow(p3.x - p1.x, 2) + Math.pow(p3.y - p1.y, 2));
+        } else if ((a == 'x' || b == 'x') && (a == 'z' || b == 'z')) {
+            p1_2 = (float) Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.z - p2.z, 2));
+            p2_3 = (float) Math.sqrt(Math.pow(p2.x - p3.x, 2) + Math.pow(p2.z - p3.z, 2));
+            p3_1 = (float) Math.sqrt(Math.pow(p3.x - p1.x, 2) + Math.pow(p3.z - p1.z, 2));
+        } else if ((a == 'y' || b == 'y') && (a == 'z' || b == 'z')) {
+            p1_2 = (float) Math.sqrt(Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z - p2.z, 2));
+            p2_3 = (float) Math.sqrt(Math.pow(p2.y - p3.y, 2) + Math.pow(p2.z - p3.z, 2));
+            p3_1 = (float) Math.sqrt(Math.pow(p3.y - p1.y, 2) + Math.pow(p3.z - p1.z, 2));
+        }
+        float radian = (float) Math.acos((p1_2 * p1_2 + p2_3 * p2_3 - p3_1 * p3_1) / (2 * p1_2 * p2_3));
+        float degree = (float) (radian / Math.PI * 180);
+        return degree;
+    }
+
+}
